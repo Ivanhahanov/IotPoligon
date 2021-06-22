a271ad3556713fdca36e33e87cfdf2ddfe2b07c1
Leonid <akatsaaaaa@gmail.com>
2021-06-22 08:42:39 -0700

    fixed bug with topics

--- redis_pubsub/pubsub_script.py
+++ redis_pubsub/pubsub_script.py
@@ -7,13 +7,8 @@ logging.basicConfig(level=logging.INFO)
 
 with open('devices.yaml', 'r') as file:
     data = yaml.safe_load(file.read())
-data = list(data)
 
-topics = []
-for t in data:
-    topics.append(t)
-print(topics)
-# topics = [row["topic"] for row in data]
+topics = [row["topic"] for row in data.values()]
 
 redis = Redis(host='redis', port=6379, db=0)
 # Subscribing to events matching pattern "__key*__:*"
@@ -29,7 +24,8 @@ while True:
         if topic in topics:
             logging.info(f"{topic}: {redis.get(topic).decode()}")
             rendered_topic = topic
+            logging.info(rendered_topic)
             if rendered_topic in topics:
-                logging.info(f'{rendered_topic} in file')
+                pass
     else:
         time.sleep(0.1)
