14192c249127674a1f7772d80578f6946d1be9d8
Leonid <akatsaaaaa@gmail.com>
2021-06-22 07:33:46 -0700

    read yaml file and payload from redis

--- redis_pubsub/pubsub_script.py
+++ redis_pubsub/pubsub_script.py
@@ -1,13 +1,20 @@
 from redis import Redis
 import time
 import logging
+import yaml
+
 logging.basicConfig(level=logging.INFO)
 
+with open('devices.yaml', 'r') as file:
+    data = yaml.safe_load(file.read())
+    #logging.info(data)
+
+topics = [row["topic"] for row in data]
 redis = Redis(host='redis', port=6379, db=0)
 # Subscribing to events matching pattern "__key*__:*"
 p = redis.pubsub()
 p.psubscribe('__keyspace@0__:*')
-topics = ['main/lamp', 'pacs/door']
+# topics = ['main/lamp', 'pacs/door']
 logging.info('Starting message loop')
 while True:
     message = p.get_message()
@@ -16,5 +23,8 @@ while True:
         topic = message['channel'].decode().split(':')[1]
         if topic in topics:
             logging.info(f"{topic}: {redis.get(topic).decode()}")
+            rendered_topic = topic
+            if rendered_topic in topics:
+                logging.info(f'{rendered_topic} in file')
     else:
         time.sleep(0.1)
